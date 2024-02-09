2c7e3ee284401cdf6c426d99a0a0a7de05116e01
Fauzan Ibrahim <fauzan.ibrahim21@gmail.com>
2024-02-10 02:47:17 +0700

    Bare skeleton to put the files

---
+++ arduino/main/main.ino
@@ -0,0 +1,11 @@
+// Don't forget to put the lib files (both cpp and h) in the main folder
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
