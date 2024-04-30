88ab364ee5f7733ac9207d10d77504f00810e9d3
skavvv <117077904+skavvv@users.noreply.github.com>
2024-04-30 17:33:35 +0700

    add left arm

--- arduino/main2/Arm.h
+++ arduino/main2/Arm.h
@@ -29,16 +29,21 @@
 class Arm {
 public:
     Arm(uint8_t stepPin1, uint8_t dirPin1, uint8_t stepPin2, uint8_t dirPin2);
+    ArmR(uint8_t stepPin1R, uint8_t dirPin1R, uint8_t stepPin2R, uint8_t dirPin2R);
 
     void move();
+    void moveR();
     void done();
     void calculate(float dx, float dy);
+    void calculateR(float dx, float dy);
 
 private:
     AccelStepper stepper;
     AccelStepper stepper2;
-    uint8_t _stepPin1, _dirPin1, _stepPin2, _dirPin2;
-    float angle1, angle2;
+    AccelStepper stepperR;
+    AccelStepper stepper2R;
+    uint8_t _stepPin1, _dirPin1, _stepPin2, _dirPin2, _stepPin1R, _dirPin1R, _stepPin2R, _dirPin2R;
+    float angle1, angle2, angle1R, angle2R;
     bool selesai;
 };
 
